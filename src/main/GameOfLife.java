63e3d611d3c2d45aff5060a1d942744c4c12b0d0
MichaPehlivan <michapehlivan2004@gmail.com>
2021-07-10 17:55:27 +0200

    restructured render code

--- src/main/GameOfLife.java
+++ src/main/GameOfLife.java
@@ -10,6 +10,7 @@ public class GameOfLife {
 	static Window window;
 	static Input input = new Input();
 	static Thread thread;
+	static BufferStrategy bs;
 	
 	//board and window setup
 	public static void start() {
@@ -18,46 +19,42 @@ public class GameOfLife {
 		window = new Window("game", 800, 500);
 		board = new Board(window.getCanvas().getWidth(), window.getCanvas().getHeight());
 		board.setUp();
-	}
-	
-	//renders the board
-	public static void render() {
-		BufferStrategy bs = window.getCanvas().getBufferStrategy();
+		
+		bs = window.getCanvas().getBufferStrategy();
 		if(bs == null) {
 			window.getCanvas().createBufferStrategy(2);
 		}
 		bs = window.getCanvas().getBufferStrategy();
-		
-		while (true) {
-			window.getCanvas().setBackground(Color.black);
-			window.requestFocusInWindow();
-			Graphics2D g2d = (Graphics2D) bs.getDrawGraphics();
+	}
+	
+	//renders the board
+	public static void render() {
+		window.getCanvas().setBackground(Color.black);
+		window.requestFocusInWindow();
+		Graphics2D g2d = (Graphics2D) bs.getDrawGraphics();
 			
-			if (!input.getPaused()) {
-				g2d.clearRect(0, 0, window.getCanvas().getWidth(), window.getCanvas().getHeight());
-				board.updateBoard();
-			}
+		if (!input.getPaused()) {
+			g2d.clearRect(0, 0, window.getCanvas().getWidth(), window.getCanvas().getHeight());
+			board.updateBoard();
+		}
 
-			g2d.setColor(Color.white);
-			for (int i = 0; i < board.getBoard().length; i++) {
-				for (int j = 0; j < board.getBoard()[0].length; j++) {					 
-					if (board.getState(i, j, board.getBoard())) {
-						g2d.fillRect(i * board.getCellSize(), j * board.getCellSize(), board.getCellSize(), board.getCellSize());
-					}
+		g2d.setColor(Color.white);
+		for (int i = 0; i < board.getBoard().length; i++) {
+			for (int j = 0; j < board.getBoard()[0].length; j++) {					 
+				if (board.getState(i, j, board.getBoard())) {
+					g2d.fillRect(i * board.getCellSize(), j * board.getCellSize(), board.getCellSize(), board.getCellSize());
 				}
 			}
-
-			g2d.dispose();
-			bs.show();
-			
-			tick();
 		}
+
+		g2d.dispose();
+		bs.show();
 	}
 	
 	//timing for framerate
-	public static void tick() {
+	public static void tick(long time) {
 		try {
-			Thread.sleep(16);
+			Thread.sleep(time);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -65,7 +62,10 @@ public class GameOfLife {
 
 	public static void main(String[] args) {
 		start();
-		render();
+		while(true) {
+			render();
+			tick(16);
+		}
 	}
 
 }
