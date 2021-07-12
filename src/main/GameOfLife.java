19e6477751438959fbebe6fee01b7fb4d4eaaf9d
MichaPehlivan <michapehlivan2004@gmail.com>
2021-07-12 15:43:11 +0200

    reverted structure and deltatime changes

--- src/main/GameOfLife.java
+++ src/main/GameOfLife.java
@@ -19,36 +19,41 @@ public class GameOfLife {
 		window = new Window("game", 800, 500);
 		board = new Board(window.getCanvas().getWidth(), window.getCanvas().getHeight());
 		board.setUp();
-		
+	}
+	
+	//renders the board
+	public static void render() {
 		bs = window.getCanvas().getBufferStrategy();
 		if(bs == null) {
 			window.getCanvas().createBufferStrategy(2);
 		}
 		bs = window.getCanvas().getBufferStrategy();
-	}
-	
-	//renders the board
-	public static void render() {
-		window.getCanvas().setBackground(Color.black);
-		window.requestFocusInWindow();
-		Graphics2D g2d = (Graphics2D) bs.getDrawGraphics();
 			
-		if (!input.getPaused()) {
-			g2d.clearRect(0, 0, window.getCanvas().getWidth(), window.getCanvas().getHeight());
-			board.updateBoard();
-		}
+		while (true) {
+			window.getCanvas().setBackground(Color.black);
+			window.requestFocusInWindow();
+			Graphics2D g2d = (Graphics2D) bs.getDrawGraphics();
+			
+			if (!input.getPaused()) {
+				g2d.clearRect(0, 0, window.getCanvas().getWidth(), window.getCanvas().getHeight());
+				board.updateBoard();
+			}
 
-		g2d.setColor(Color.white);
-		for (int i = 0; i < board.getBoard().length; i++) {
-			for (int j = 0; j < board.getBoard()[0].length; j++) {					 
-				if (board.getState(i, j, board.getBoard())) {
-					g2d.fillRect(i * board.getCellSize(), j * board.getCellSize(), board.getCellSize(), board.getCellSize());
+			g2d.setColor(Color.white);
+			for (int i = 0; i < board.getBoard().length; i++) {
+				for (int j = 0; j < board.getBoard()[0].length; j++) {
+					if (board.getState(i, j, board.getBoard())) {
+						g2d.fillRect(i * board.getCellSize(), j * board.getCellSize(), board.getCellSize(),
+								board.getCellSize());
+					}
 				}
 			}
-		}
 
-		g2d.dispose();
-		bs.show();
+			g2d.dispose();
+			bs.show();
+			
+			tick(16);
+		}
 	}
 	
 	//timing for framerate
@@ -63,13 +68,7 @@ public class GameOfLife {
 	//main loop
 	public static void main(String[] args) {
 		start();
-		while(true) {
-			long start = System.currentTimeMillis();
-			render();
-			long end = System.currentTimeMillis();
-			long delta = end - start;
-			tick(Math.max(0, 16-delta));
-		}
+		render();
 	}
 
 }
